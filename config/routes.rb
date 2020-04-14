a7966bdf9361e3febbe2e0b650abccf579ca6459
whitesllgit <t-nisioka@hotmail.com>
2020-04-14 23:50:35 +0900

    routes テスト2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root  'users#index'  #この1行を追加
+  root  'posts#index'
 end
