84099ee494e7a7e4e953a2015291f3b7e64ebbbe
whitesllgit <t-nisioka@hotmail.com>
2020-04-15 23:10:33 +0900

    自動デプロイ テスト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,3 +92,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+server '18.178.95.49', user: 'ec2-user', roles: %w{app db web}
