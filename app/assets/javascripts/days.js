121ac1189c0c53f915912df4d10f66c499d8ab66
nishiukei <nisiukei@yahoo.co.jp>
2020-04-25 20:47:41 +0900

    input-form4

---
+++ app/assets/javascripts/days.js
@@ -0,0 +1,8 @@
+(function() {
+  "use strict";
+
+  const select_year = document.getElementById("select_year");
+  const select_month = document.getElementById("select_month");
+  const select_day = document.getElementById("select_day");
+  
+})
