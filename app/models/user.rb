c9f40fb75ba4fbaaeed3adc64183a616997ad2e0
nishiukei <nisiukei@yahoo.co.jp>
2020-05-03 18:49:54 +0900

    validation1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, :nickname, :full_width_last_name, :full_width_first_name,
+            :hira_last_name,:hira_first_name, :birth_date, presence: true
+
+  validates :full_width_last_name, :full_width_first_name,
+            :hira_last_name,:hira_first_name, format: {with: /[^a-zA-Z0-9]/ }
 
   has_many :products
   has_many :comments
