be378596eb7c5d90f031017692e08d41682554dd
nishiukei <nisiukei@yahoo.co.jp>
2020-05-04 17:33:14 +0900

    login-logout-setting

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,8 +3,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  prepend_before_action :require_no_authentication, :only => [ :cancel]
-  prepend_before_action :authenticate_scope!, :only => [:new, :create ,:edit, :update, :destroy]
+  # prepend_before_action :require_no_authentication, :only => [ :cancel]
+  # prepend_before_action :authenticate_scope!, :only => [:new, :create ,:edit, :update, :destroy]
 
 
   # GET /resource/sign_up
