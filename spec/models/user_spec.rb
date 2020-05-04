e0ae5503ec33af812dd8fd29def7bb4af6ccaee8
nishiukei <nisiukei@yahoo.co.jp>
2020-05-04 22:27:53 +0900

    test1

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+describe User do
+  describe "#create" do
+    it "ニックネームがない場合は登録できないこと" do
+      user = User.new(nickname: "", email: "aaaaa@gmail", password: "0000000",password_confirmation: "0000000", full_width_last_name: "あああ", full_width_first_name: "あああ", hira_last_name: "あああ", hira_first_name:"あああ", birth_date: "1991-08-21")
+      user.valid?
+      expect(user.errors[:nickname]).to include
+    end
+  end
+end
