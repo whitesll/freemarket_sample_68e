e2e755e121ec5224bf74287210945dee5198b308
tyasu106 <t.yasu106@gmail.com>
2020-05-06 22:19:47 +0900

    category-select

--- db/migrate/20200502055755_create_products.rb
+++ db/migrate/20200502055755_create_products.rb
@@ -9,7 +9,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string      :brand
       t.integer     :saler_id,        null: false
       t.integer     :buyer_id,        null: false
-      t.integer     :status,          null: false
+      t.string      :status,          null: false
       t.string      :shippingcharges, null: false
       t.string      :shipping_area,   null: false
       t.string      :days_to_ship,    null: false
