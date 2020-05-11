6af28093fba3731a728da4ae752a633eef5758e9
tyasu106 <t.yasu106@gmail.com>
2020-05-11 23:13:59 +0900

    upp

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @product.images.new
+    @categories = Category.all
   end
 
   def create
@@ -35,5 +36,5 @@ end
 private
 
 def product_params
-  params.require(:product).permit(:name,:text,:category_id,:price,:status,:shippingcharges,:shipping_area,:days_to_ship, images_attributes: [:image])
+  params.require(:product).permit(:name,:text,:category_id,:price,:status,:shippingcharges,:shipping_area,:days_to_ship, images_attributes: [:image, :_destroy, :id]) .merge(user_id: 1).merge(saler_id: 1).merge(buyer_id: 1)
 end
