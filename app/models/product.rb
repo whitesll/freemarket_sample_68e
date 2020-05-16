0a3cb15cd08850f0941411423a8f06007cf8e979
tyasu106 <t.yasu106@gmail.com>
2020-05-16 21:33:24 +0900

    Prefecture

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ApplicationRecord
   has_many :comments
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
