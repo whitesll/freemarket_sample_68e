de1b1ffcdbac8843469291fa77fc6783c4ac0052
nishiukei <nisiukei@yahoo.co.jp>
2020-05-29 22:08:48 +0900

    test-prroduct

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :product do
-    
+    image            Rack::Test::Uploadedfile.new(File.join(Rails.root, 'spec/fixtures/test_image.jpg'))
+    name             {"Tシャツ"}
+    text             {"サイズが合わず、新品同様です。"}
+    status           {"未使用に近い"}
+    shippingcharges  {"送料込み(出品者負担)"}
+    shipping_area    {"福岡県"}
+    days_to_ship     {"2~3日で発送"}
+    price            {"1500"}
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Product do
+  describe "nil:false" do
+    it "全ての項目が入力されていれば登録できること" do
+      product = build(:product)
+      expect(product).to be_valid
+    end
+  end
 end
