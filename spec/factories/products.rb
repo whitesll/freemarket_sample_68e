4d7079c4fd882c852c56365b4fb6ac368a4a4308
nishiukei <nisiukei@yahoo.co.jp>
2020-05-30 17:17:03 +0900

    テスト修正

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :product do
-
     user
     name             {"Tシャツ"}
     text             {"サイズが合わず、新品同様です。"}
     brand            {"モダン"}
-    category
+    category         
     status           {"目立った傷や汚れなし"}
     shippingcharges  {"送料込み(出品者負担)"}
     shipping_area    {"福岡県"}
@@ -15,4 +14,22 @@ FactoryBot.define do
       product.images << build(:image, product: product)
     end
   end
+
+  factory :product_images do
+    user
+    name             {"Tシャツ"}
+    text             {"サイズが合わず、新品同様です。"}
+    brand            {"モダン"}
+    category         
+    status           {"目立った傷や汚れなし"}
+    shippingcharges  {"送料込み(出品者負担)"}
+    shipping_area    {"福岡県"}
+    days_to_ship     {"2~3日で発送"}
+    price            {"1500"}
+
+    after(:build) do |product|
+      product.images << build(:image_10, 10, product: product)
+    end
+  end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,5 +6,58 @@ describe Product do
       product = build(:product)
       expect(product).to be_valid
     end
+    it "画像がない場合保存できない" do
+      product = build(:product, images: "")
+      product.valid?
+      expect(product.errors[:image]).to include("を入力してください")
+    end
+    it "商品名がない場合保存できない" do
+      product = build(:product, name: "")
+      product.valid?
+      expect(product.errors[:name]).to include("を入力してください")
+    end
+    it "商品の説明がない場合保存できない" do
+      product = build(:product, text: "")
+      product.valid?
+      expect(product.errors[:text]).to include("を入力してください")
+    end
+    it "カテゴリーがない場合保存できない" do
+      product = build(:product, category: "")
+      product.valid?
+      expect(product.errors[:category]).to include("を入力してください")
+    end
+    it "商品の状態がない場合保存できない" do
+      product = build(:product, status: "")
+      product.valid?
+      expect(product.errors[:status]).to include("を入力してください")
+    end
+    it "配送料の負担がない場合保存できない" do
+      product = build(:product, shippingcharges: "")
+      product.valid?
+      expect(product.errors[:shippingcharges]).to include("を入力してください")
+    end
+    it "配送元の地域がない場合保存できない" do
+      product = build(:product, shipping_area: "")
+      product.valid?
+      expect(product.errors[:shipping_area]).to include("を入力してください")
+    end
+    it "発送までの日数がない場合保存できない" do
+      product = build(:product, days_to_ship: "")
+      product.valid?
+      expect(product.errors[:days_to_ship]).to include("を入力してください")
+    end
+    it "価格がない場合保存できない" do
+      product = build(:product, price: "")
+      product.valid?
+      expect(product.errors[:price]).to include("を入力してください")
+    end
+  end
+
+  describe "image" do
+    it "画像が10枚あるとき保存できない" do
+      product = build(:product_images)
+      product.valid?
+      expect(product.errors[:image]).to include("を入力してください")
+    end
   end
 end
