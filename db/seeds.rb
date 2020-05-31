e6fbd251315824b664c73d4683e77e23a9f2ff13
whitesllgit <t-nisioka@hotmail.com>
2020-05-31 17:16:25 +0900

    カテゴリーのseedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lady = Category.create(name: "レディース")
+man = Category.create(name: "メンズ")
+baby = Category.create(name: "ベビー・キッズ")
+interior = Category.create(name: "インテリア・住まい・小物")
+book= Category.create(name: "本・音楽・ゲーム")
+hobby = Category.create(name: "おもちゃ・ホビー・グッズ")
+cosmetics = Category.create(name: "コスメ・香水・美容")
+appliance = Category.create(name: "家電・スマホ・カメラ")
+sport = Category.create(name: "スポーツ・レジャー")
+handmade = Category.create(name: "ハンドメイド")
+ticket = Category.create(name: "チケット")
+bicycle = Category.create(name: "自転車・オートバイ")
+others = Category.create(name: "その他")
