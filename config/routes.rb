09e5818f3f65628e57bf9278ad7f35c10b8853f0
whitesllgit <t-nisioka@hotmail.com>
2020-06-02 21:07:11 +0900

    商品購入機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'purchase/index'
+  get 'purchase/done'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -14,7 +16,14 @@ Rails.application.routes.draw do
       get 'edit_address', to: 'users#edit_address'
       patch 'edit_address', to: 'users#edit_address_post'
     end
-    resources :creditcards
+    resources :creditcards, only: [:new, :show] do
+      collection do
+        get 'new', to: 'creditcards#new'
+        post 'show', to: 'creditcards#show'
+        post 'pay', to: 'creditcards#pay'
+        post 'delete', to: 'creditcards#delete'
+      end
+    end
   end
   
   root to: 'products#index'
@@ -22,6 +31,13 @@ Rails.application.routes.draw do
     collection do
       get :buy_confirmation
       get '/', to: 'products#new'
+    end
+  end
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
     end
   end
 end
