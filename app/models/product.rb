f81c59f3468527d899d792a81e8cb4bc12a4e86f
nishiukei <nisiukei@yahoo.co.jp>
2020-06-07 20:50:59 +0900

    edit4

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   has_many :comments
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+  
   validates :images,           presence: true, length: {maximum: 10}
   validates :name,             presence: true, length: {maximum: 40}
   validates :text,             presence: true, length: {maximum: 1000}
@@ -13,6 +14,9 @@ class Product < ApplicationRecord
   validates :shippingcharges,  presence: true
   validates :shipping_area,    presence: true
   validates :days_to_ship,     presence: true
-  validates :price,            presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} 
+  validates :price,            presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 
 end
