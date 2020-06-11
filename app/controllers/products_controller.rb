9626411a9c588305d8bc84e5f010c133eb58a629
mattun3 <60844878+mattun3@users.noreply.github.com>
2020-06-11 23:01:13 +0900

    Merge branch 'master' into category-list

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, except: [:index, :new, :create]
+  skip_before_action :set_product, only: [:get_category_children, :get_category_grandchildren]
+  
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
@@ -10,6 +12,23 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @product.images.new
+    @categories = Category.all
+    #データベースから、親カテゴリーのみ抽出し、配列化
+    @category_parent_array = Category.where(ancestry: nil)
+  end
+
+  # 以下全て、formatはjsonのみ
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    # ここでfind_byを使うことでレディーしか取れてなかった
+    @category_children = Category.find(params[:parent_id]).children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
   def create
@@ -22,6 +41,8 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
+    @category = @product.category
   end
 
   def edit
