de5a72211427ca52e804108602829e45cce24f66
whitesllgit <t-nisioka@hotmail.com>
2020-06-16 15:41:12 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,6 @@ Rails.application.routes.draw do
   end
   resources :purchase, only: [:index] do
     collection do
-      get 'index', to: 'purchase#index'
       post 'pay', to: 'purchase#pay'
       get 'done', to: 'purchase#done'
     end
