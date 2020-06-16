cfa4c9b7c4ea5ec4fd047d23467f5259a349fa43
nishiukei <nisiukei@yahoo.co.jp>
2020-06-17 03:11:51 +0900

    mypage-js3

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,20 +1,15 @@
-// $(function(){
-//   $(".mypage_side_box").on("click",function(e){
-//     e.preventDefault();
-//     console.log("hello world");
-//   })
-// });
-window.onpageshow = function(event) {
-  if (event.persisted) {
-    window.location.reload();
-  }
-};
 $(function() {
   let tabs = $(".mypage_side_box");
+
   $(document).on("click",".mypage_side_box", function() {
     $(".active").removeClass("active"); 
     $(this).addClass("active");
-    var index = tabs.index(this);
+    const index = tabs.index(this);
     $(".main_display_mypage_box").removeClass("show").eq(index).addClass("show");
   });
+
+  $(".go_to_mypage").on("click",function(){
+    window.location.reload("/users/:id");
+  });
+  
 });
